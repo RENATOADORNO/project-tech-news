84b3d6b26c52231881279c3302223b5235a56055
Renato Adorno <renattoadorno@gmail.com>
2022-09-22 13:58:44 -0300

    feat: adds ratings

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,24 @@
+from tech_news import database
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = database.find_news()
+    results = []
+    order = sorted(news, key=lambda n: (-n["comments_count"], n["title"]))
+
+    for n in order:
+        results.append((n["title"], n["url"]))
+
+    return results[:5]
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = database.find_news()
+    categories = [new["category"] for new in news]
+    sort_categories = sorted(
+        set(categories),
+        key=lambda category: (-categories.count(category), category)
+    )
+    return sort_categories[:5]
